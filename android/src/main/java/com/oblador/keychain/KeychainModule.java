fa0059e6bf4e633bc647d9639e5458c317e950e1
Cory Smith <cory.m.smith@gmail.com>
2016-02-12 12:34:24 -0700

    Updates to Android storage

--- android/src/main/java/com/oblador/keychain/KeychainModule.java
+++ android/src/main/java/com/oblador/keychain/KeychainModule.java
@@ -22,69 +22,87 @@ import java.nio.charset.Charset;
 
 public class KeychainModule extends ReactContextBaseJavaModule {
 
-  public static final String REACT_CLASS = "RNKeychainManager";
-  public static final String KEYCHAIN_DATA = "RN_KEYCHAIN";
+    public static final String REACT_CLASS = "RNKeychainManager";
+    public static final String KEYCHAIN_DATA = "RN_KEYCHAIN";
 
-  private final Crypto crypto;
-  private final SharedPreferences prefs;
+    private final Crypto crypto;
+    private final SharedPreferences prefs;
 
-  @Override
-  public String getName() {
-    return REACT_CLASS;
-  }
-
-  public KeychainModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    crypto = new Crypto(
-      new SharedPrefsBackedKeyChain(getReactApplicationContext()),
-      new SystemNativeCryptoLibrary());
-    prefs = this.getReactApplicationContext().getSharedPreferences(KEYCHAIN_DATA, Context.MODE_PRIVATE);
-  }
-
-  @ReactMethod
-  public void setGenericPasswordForService(String service, String username, String password, Callback callback) {
-    if (!crypto.isAvailable()) {
-      Log.e("Crypto", "Crypto is missing");
+    @Override
+    public String getName() {
+        return REACT_CLASS;
     }
-    //TODO: Service name?
-    Entity entity = new Entity(KEYCHAIN_DATA + ":" + service + ":" + password);
-    byte[] encryptedPass = new byte[0];
-    byte[] b = password.getBytes(Charset.forName("UTF-8"));
-    try {
-      encryptedPass = crypto.encrypt(b, entity);
-      byte[] decryptedPass = crypto.decrypt(encryptedPass, entity);
-      //TODO: Store both in SharedPreferences?
 
-      SharedPreferences.Editor prefsEditor = prefs.edit();
-      prefsEditor.putString()
-
-      prefsEditor.commit();
-      Log.e("Crypto Encrypted", new String(encryptedPass));
-      Log.e("Crypto Decrypted ", new String(decryptedPass));
-    } catch (KeyChainException e) {
-      e.printStackTrace();
-    } catch (CryptoInitializationException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+    public KeychainModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        crypto = new Crypto(
+                new SharedPrefsBackedKeyChain(getReactApplicationContext()),
+                new SystemNativeCryptoLibrary());
+        prefs = this.getReactApplicationContext().getSharedPreferences(KEYCHAIN_DATA, Context.MODE_PRIVATE);
     }
-  }
 
-  @ReactMethod
-  public void getGenericPasswordForService(String service, Callback successCallback, Callback errorCallback) {
+    @ReactMethod
+    public void setGenericPasswordForService(String service, String username, String password, Callback callback) {
+        if (!crypto.isAvailable()) {
+            Log.e("Crypto", "Crypto is missing");
+        }
 
-    String username = prefs.getString(service+":username", "");
-    String password = prefs.getString(service+":password", "");
+        Entity entity = new Entity(KEYCHAIN_DATA + ":" + service);
 
-    successCallback.invoke(username, password);
-  }
+        try {
+            String encryptedUsername = encryptWithEntity(username, entity);
+            String encryptedPassword = encryptWithEntity(username, entity);
 
-  @ReactMethod
-  public void resetGenericPasswordForService(String service) {
+            SharedPreferences.Editor prefsEditor = prefs.edit();
+            prefsEditor.putString(service + ":u", encryptedUsername);
+            prefsEditor.putString(service + ":p", encryptedPassword);
+            prefsEditor.apply();
+            Log.e("Crypto Encrypted", encryptedUsername +":"+encryptedPassword);
+            callback.invoke(null);
+        } catch (Exception e) {
+            callback.invoke(e);
+        }
+    }
 
-  }
+    private String encryptWithEntity(String string, Entity entity) {
+        try {
+            byte[] encrypted = new byte[0];
+            byte[] bytes = string.getBytes(Charset.forName("UTF-8"));
+            return new String(crypto.encrypt(bytes, entity));
+        } catch (Exception e) {
+            //callback.invoke(e);
+            return null;
+        }
+    }
 
-  private String getSharedPrefKey(String service, )
+    @ReactMethod
+    public void getGenericPasswordForService(String service, Callback callback) {
+
+        String username = prefs.getString(service + ":u", "");
+        String password = prefs.getString(service + ":p", "");
+
+        Entity entity = new Entity(KEYCHAIN_DATA + ":" + service + ":" + password);
+
+        try {
+            byte[] decryptedUsername = crypto.decrypt(username.getBytes(), entity);
+            byte[] decryptedPass = crypto.decrypt(password.getBytes(), entity);
+            Log.e("Crypto Decrypted u: ", new String(decryptedUsername));
+            Log.e("Crypto Decrypted p: ", new String(decryptedPass));
+            callback.invoke(null, new String(decryptedUsername), new String(decryptedPass));
+        } catch (Exception e) {
+            e.printStackTrace();
+            callback.invoke(e);
+        }
+    }
+
+    @ReactMethod
+    public void resetGenericPasswordForService(String service) {
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.remove(service + ":u");
+        prefsEditor.remove(service + ":p");
+        prefsEditor.apply();
+    }
+
 
 //    @ReactMethod
 //    public void setInternetCredentialsForServer(String server, String username, String password, Callback callback) {
@@ -100,10 +118,6 @@ public class KeychainModule extends ReactContextBaseJavaModule {
 //    public void resetInternetCredentialsForServer(String server, Callback callback) {
 //
 //    }
-
-
-
-
 
 
 }
